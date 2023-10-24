a81c0aa11098d9453e86e2f06a7821dbfd375a90
Dmitry Turchenko <dmitrytyt@ya.ru>
2023-10-24 22:22:40 +0300

    Add 4-navigator

---
+++ 4-navigator/navigator.js
@@ -0,0 +1,7 @@
+const positionLat = 2;
+const positionLong = 4;
+const addresLat = 3;
+const addresLong = 6;
+
+const result = Math.sqrt((addresLat - positionLat)**2 + (addresLong - positionLong)**2);
+console.log(result);
