1423f32e9f0718bb90067156f7ee5cb56e1b02d2
Dmitry Turchenko <dmitrytyt@ya.ru>
2023-11-05 13:50:45 +0300

    Fix exeption error

--- 12-luna/luna.js
+++ 12-luna/luna.js
@@ -1,14 +1,21 @@
 function cleaner(str) {
-    if (typeof str === 'string' || str instanceof String) {
-        return str.toString().replace(/-/g, '');
+    try {
+        if (typeof str === 'string' || str instanceof String) {
+            return str.toString().replace(/-/g, '');
+        } else {
+            throw new SyntaxError('Not a string was passed');
+        }
+    } catch (e) {
+        console.error("String Error: " + e.message);
     }
-
-    return '';
 }
 
 function validate(strNum) {
-    const parity = strNum.length % 2;
+    if (strNum.length === 0) {
+        return false;
+    }
 
+    const parity = strNum.length % 2;
     let sum = 0;
 
     for (let i = 0; i < strNum.length; i++) {
@@ -29,3 +36,6 @@ function validate(strNum) {
 }
 
 console.log(validate(cleaner('4561-2612-1234-5467')));
+console.log(validate(cleaner('fsdf-fsdf-dsaf')));
+console.log(validate(cleaner('')));
+console.log(validate(cleaner(2)));
